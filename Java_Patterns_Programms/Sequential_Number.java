43bcff790a08bf892da54d074d20323b01fcd472
song recommendation <prashantpatil4524@gmail.com>
2026-04-03 15:35:55 +0530

    New Pattern and Basic Programms

---
+++ Java_Patterns_Programms/Sequential_Number.java
@@ -0,0 +1,20 @@
+// Sequence Number Pattern Program in Java
+
+class Sequential_Number
+{
+    public static void main(String args[])
+    {
+        int n = 5;
+        int k = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                System.out.printf("%2d ", k);
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Java_basic_programms/Ever_Odd.java
@@ -0,0 +1,15 @@
+// 9. Write a program to check whether a number is Even or Odd.
+import java.util.Scanner;
+public class Ever_Odd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if (num % 2 == 0) {
+            System.out.println(num + " is an Even number.");
+        } else {
+            System.out.println(num + " is an Odd number.");
+        }
+        sc.close();
+    }
+}
