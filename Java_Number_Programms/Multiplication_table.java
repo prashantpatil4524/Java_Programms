cb9aa93b8699f5efb791c0970a35f8fc045ed262
song recommendation <prashantpatil4524@gmail.com>
2026-04-18 20:21:39 +0530

    Java Number and Pattern Programms

---
+++ Java_Number_Programms/Multiplication_table.java
@@ -0,0 +1,15 @@
+// 24. Write a program to print the multiplication table of a number.
+
+import java.util.Scanner;
+public class Multiplication_table {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Multiplication Table of " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
