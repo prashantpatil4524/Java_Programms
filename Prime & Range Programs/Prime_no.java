b6a484c7fee373c9ec6c197c2479a0fe3ebda18c
song recommendation <prashantpatil4524@gmail.com>
2026-04-20 18:00:01 +0530

    New Range Programms

---
+++ Prime & Range Programs/Prime_no.java
@@ -0,0 +1,28 @@
+// Write a program to print prime numbers from 1 to n.
+import java.util.Scanner;
+public class Prime_no {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Prime numbers from 1 to " + n + ":");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
