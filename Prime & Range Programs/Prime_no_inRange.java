b39623d8460f47c28154b38ade0e7b4e824b754c
song recommendation <prashantpatil4524@gmail.com>
2026-04-20 18:10:08 +0530

    New Prime In range

---
+++ Prime & Range Programs/Prime_no_inRange.java
@@ -0,0 +1,32 @@
+// 26. Write a program to print prime numbers between n and k.
+
+import java.util.Scanner;
+public class Prime_no_inRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the value of k: ");
+        int k = sc.nextInt();
+
+        System.out.println("Prime numbers between " + n + " and " + k + ":");
+        for (int i = n; i <= k; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
