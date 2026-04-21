536c285746ab30b62b73e2a2747a9e2df7bf5acd
song recommendation <prashantpatil4524@gmail.com>
2026-04-21 17:08:36 +0530

    Array Programms and Patterns

---
+++ Array_Programs/Max_element.java
@@ -0,0 +1,29 @@
+// 27. Write a program to find the maximum element in an array.
+
+import java.util.Scanner;
+public class Max_element {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = findMax(arr);
+        System.out.println("The maximum element in the array is: " + max);
+    }
+
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
---
+++ Java_Patterns_Programms/Star_Square.java
@@ -0,0 +1,18 @@
+// Java program to print a square pattern of stars
+
+
+class Star_Square {
+    public static void main(String args[]) {
+        int n = 5; // size
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ((j == 1 || j == n) || (i == 1 || i == n))
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
